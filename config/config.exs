9d62ae4c1681b73f98b8b7f194487823b6acd9c7
Carlo Sana <sana.carlo@gmail.com>
2021-03-06 10:06:21 +0100

    phoenix working

--- config/config.exs
+++ config/config.exs
@@ -16,3 +16,38 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+
+# PHOENIX APP Config
+
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+# use Mix.Config
+
+config :web_frontend,
+  ecto_repos: [WebFrontend.Repo]
+
+# Configures the endpoint
+config :web_frontend, WebFrontendWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "Wq0ZUzndhDivs8tGPW5A51rn+e3KEk1agwRVhQocv1u1X8jTuGx9sHoNSM8N82OX",
+  render_errors: [view: WebFrontendWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: WebFrontend.PubSub,
+  live_view: [signing_salt: "fsXBjnv6"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
